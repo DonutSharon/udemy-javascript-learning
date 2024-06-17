268cdc8fb287e8f7d090877b04703e4289c07bc8
Sharon Maldonado <sharonmaldonado4387@gmail.com>
2024-06-17 13:10:36 -0400

    added more array stuff

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 let shoppingList = ['apple sapling', 'peach sapling'];
+
+
+// push - add to end
+// pop - remove from end
+// unshift - add to start
+// shift - remove from start 
+
+let topSongs = [
+    'Armagedon',
+    'Heya',
+    'Fri[end]s',
+    'Wife'
+];
+
+// add to end - push - changing length of array
+topSongs.push('Supernova')
+
+// Array Methods
+/**
+ * concat
+ * includes
+ * indexOf
+ * join
+ * reverse
+ * slice
+ * splice
+ * sort
+*/
